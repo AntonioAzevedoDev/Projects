985bc29920412b658ced6287a008f1d1b53ac78b
DarknerssVPS <57262360+DarknerssVPS@users.noreply.github.com>
2020-07-08 16:53:05 -0300

    Delicias da Auzi

--- delicias_da_auzi01/android/app/src/main/java/br/com/lucas/delicias_da_auzi01/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package br.com.lucas.delicias_da_auzi01;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ delicias_da_auzi1/android/app/src/main/kotlin/br/com/lucas/delicias_da_auzi/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.lucas.delicias_da_auzi
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+class MainActivity: FlutterActivity() {
+
+
+}
